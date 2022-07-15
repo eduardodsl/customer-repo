bacead4ed661ba46452de9adf6ab3a8256daa675
eduardo <eduardodsl@gmail.com>
2022-07-15 18:55:48 -0300

    first commit

---
+++ src/test/java/br/com/dsleite/customerrepo/CustomerRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dsleite.customerrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
